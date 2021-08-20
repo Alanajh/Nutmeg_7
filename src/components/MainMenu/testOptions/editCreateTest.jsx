44bcf65203800de99ba3f26050a4bd65147054d8
Alana Harmon <alana.harmon@yahoo.com>
2021-08-20 16:54:03 -0500

    Ccreate and list buttons are working

--- src/components/MainMenu/testOptions/editCreateTest.jsx
+++ src/components/MainMenu/testOptions/editCreateTest.jsx
@@ -8,52 +8,30 @@ class EditCreateTests extends Component {
     constructor(props) {
     super(props);
     this.state = {
+        createComponent: false,
         testScreen: false,
         value: 'list'
     };
-
-    this.chooseTestEvent = this.chooseTestEvent.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  componentDidMount = () => {
-    
-}
-  chooseTestEvent(event) {
-    this.setState({value: event.target.value});
-    /* if(this.state.value === 'list'){
-        this.setState({ testScreen: true });
-    } */
   }
 
-  handleSubmit(event) {
-    const testForm = document.getElementById('testForm');
-    const testPage = document.getElementById('pgTestpage');
-    const createComp = document.getElementById('pgCreateComponent');
-    if(this.state.value === 'list'){
-        this.setState({ testScreen: true });
-        testForm.remove();
-        createComp.remove();
-    } else if(this.state.value === 'create'){
-        this.setState({ createComponent: true });
-        testForm.remove();
-        testPage.remove();
-    }
+  listBtn = () => {
+    this.setState({ testScreen: true });
+    this.setState({ createComponent: false });
+  }
+  createBtn = () => {
+    this.setState({ testScreen: false });
+    this.setState({ createComponent: true });
+  }
+  editBtn = () => {
+
   }
     render() {
         return <div>
-            <form onSubmit={this.handleSubmit} id='testForm'>
-                <label>
-                Tests: 
-                </label>
-                <select value={this.state.value} onChange={this.chooseTestEvent}>
-                    <option value="list" id="opt"></option>
-                    <option value="list" id="opt">List</option>
-                    <option value="create" id="opt">Create</option>
-                    <option value="edit" id="opt">Edit</option>
-                </select>
-               
-                <input type="submit" value="Submit" id='testSelectSubmit'/>
-            </form>
+            <div id="group_TestBtns">
+              <button id="testOptionsBtn" onClick={this.listBtn}>List</button>
+              <button id="testOptionsBtn" onClick={this.createBtn}>Create</button>
+              <button id="testOptionsBtn">Edit</button>
+            </div>
             <div>
             <div id="pgTestpage">{this.state.testScreen ? <TestPage/> : null }</div>
             <div id="pgCreateComponent">{this.state.createComponent ? <CreateTests/> : null }</div>
