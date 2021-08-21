158f551c99d8b23abe6f9f334fe41af82dd0288e
Alana Harmon <alana.harmon@yahoo.com>
2021-08-20 22:33:50 -0500

    Can add questions and title - but no database yet

--- src/components/MainMenu/testOptions/createComponent.jsx
+++ src/components/MainMenu/testOptions/createComponent.jsx
@@ -1,13 +1,29 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 import '../../../css/takeTest.css';
 import Menu from '../mainMenuScreen';
 
+const QuestionsList = () => {
+    const [questions, setQuestions] = useState('');
+    const [adding, setAdding] = useState([]);
+    const onClick = () => {
+        setQuestions('');
+        setAdding([...adding, questions]);
+    }
+    return <div>
+        <input value={questions} onChange={(e) => setQuestions(e.target
+        .value)}/>
+        <button onClick={onClick}>Add Question</button>
+        <ul>{adding.map(item => <li>{item}</li>)}</ul>
+    </div>
+}
 
 class CreateTests extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
+            questionsAddedArray: [],
             testScreen: false
         };
       }
@@ -24,17 +40,31 @@ class CreateTests extends Component {
             this.setState({ testScreen: false});
         }
     }
+
+    addTestHeader = () => {
+        let title = document.getElementById('createdTitle').value;
+        let genre = document.getElementById('createdGenre').value;
+        document.getElementById('createdTestTitle').innerHTML = title;
+        document.getElementById('selectedTestGenre').innerHTML = genre;
+        if (!title){
+           document.getElementById('warningLbl').innerHTML = 'Please enter a valid title and genre';
+           setTimeout(function(){document.getElementById('warningLbl').innerHTML = ''}, 3000);
+        }
+    }
+   
     render(){
         return <div>
             <div>{ this.state.testScreen ? <Menu/> : null }</div>
             <table id='testTbl'>
             <tr id='mainData'>
                 <td></td>
-                <td><label>Title:</label>
-                    <input></input>
+                <td>
+                    <label>Title:</label>
+                    <input id="createdTitle"></input>
                     <p/>
                     <label>Genre:</label>
-                    <input></input></td>
+                    <input id="createdGenre"></input>
+                </td> {/* dropdown of selected item from list of genres  */}
                 <td></td>
             </tr>
             <tr>
@@ -44,10 +74,42 @@ class CreateTests extends Component {
             </tr>
             <tr>
                 <td></td>
-                <td><button>Add</button></td>
+                <td><label id="warningLbl"></label></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td id="addBtnContainer">
+                    <button id="addBtn" onClick={this.addTestHeader}>Create Title</button>
+                </td>
             </tr>
         </table>
-        </div>
+            
+            <table>
+                <tr>
+                    <th></th>
+                    <th>
+                        <div>
+                        Test title:<label id="createdTestTitle"></label><br/>
+                        Test genre:<label id="selectedTestGenre"></label>
+                        </div>
+                    </th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+            <table id="QA">
+                <tr>
+                <td><ul>
+                        <QuestionsList/>
+                   </ul></td>
+                </tr>
+            </table>
+       </div>
     }
 
 }
--- src/components/smallComponents/menuBtn.jsx
+++
@@ -1,7 +0,0 @@
-export const menuBtn = () => {
-    return  <div id="group_TestBtns">
-    <button id="testOptionsBtn" onClick={this.listBtn}>List</button>
-    <button id="testOptionsBtn" onClick={this.createBtn}>Create</button>
-    <button id="testOptionsBtn">Edit</button>
-  </div>
-}
